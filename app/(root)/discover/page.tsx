04683c873df1ecc6cd0c8e9b0d40a9d5ffa84b06
Hardik Arora <hardikarora826@gmail.com>
2024-07-02 23:33:25 +0530

    search bar

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+"use client";
 
-const Discover = () => {
+import EmptyState from "@/components/EmptyState";
+import LoaderSpinner from "@/components/LoaderSpinner";
+import PodcastCard from "@/components/PodcastCard";
+import SearchBar from "@/components/SearchBar";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import React from "react";
+
+const Discover = ({
+  searchParams: { search },
+}: {
+  searchParams: { search: string };
+}) => {
+  const podcastsData = useQuery(api.podcasts.getPodcastBySearch, {
+    search: search || "",
+  });
   return (
-    <div>
-        <h1 className='text-20 font-bold text-white-1'>Discover</h1>
+    <div className="flex flex-col gap-9">
+      <SearchBar />
+      <div className="flex flex-col gap-9">
+        <h1 className="text-20 font-bold text-white-1">Discover</h1>
+        {podcastsData ? (
+          <>
+            {podcastsData.length > 0 ? (
+              <div className="podcast_grid">
+                {podcastsData?.map(
+                  ({ _id, podcastTitle, podcastDesc, imageUrl }) => {
+                    return (
+                      <PodcastCard
+                        key={_id}
+                        imgUrl={imageUrl!}
+                        title={podcastTitle}
+                        description={podcastDesc}
+                        podcastId={_id}
+                      />
+                    );
+                  }
+                )}
+              </div>
+            ) : (
+              <EmptyState title="No podcasts found" />
+            )}
+          </>
+        ) : (
+          <LoaderSpinner />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Discover
+export default Discover;
---
+++ components/SearchBar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+
+const SearchBar = () => {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+  const pathName = usePathname();
+
+  useEffect(() => {
+    if (search) {
+      router.push(`/discover?search=${search}`);
+    } else if (!search && pathName === "/discover") {
+      router.push(`/discover`);
+    }
+  }, [router, pathName, search]);
+
+  
+  return (
+    <div className="relative mt-8 block">
+      <Input
+        className="input-class py-6 pl-12 focus-visible:ring-offset-orange-1"
+        placeholder="Search for podcasts"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        onLoad={() => setSearch("")}
+      />
+      <Image
+        src="/icons/search.svg"
+        className="absolute left-4 top-3.5"
+        width={20}
+        height={20}
+        alt="search"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
