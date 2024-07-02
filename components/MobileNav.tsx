d925494f64f5beba432759d971d19c87948f5e7c
Hardik Arora <hardikarora826@gmail.com>
2024-07-03 00:23:16 +0530

    mobile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,9 +1,80 @@
-import React from 'react'
+"use client";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const MobileNav = () => {
+  const pathName = usePathname();
   return (
-    <div>MobileNav</div>
-  )
-}
+    <section>
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            height={30}
+            width={30}
+            alt="menu"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-black-1">
+          <SheetClose asChild>
+            <Link
+              href="/"
+              className="flex cursor-pointer items-center gap-1 pb-10 pl-4"
+            >
+              <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+              <h1 className="text-24 text-white-1 ml-2 font-extrabold">
+                Podcastrr
+              </h1>
+            </Link>
+          </SheetClose>
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 text-white-1">
+                {sidebarLinks.map(({ route, label, imgURL }) => {
+                  const isActive =
+                    pathName === route || pathName.startsWith(`${route}/`);
 
-export default MobileNav
+                  return (
+                    <SheetClose asChild key={route}>
+                      <Link
+                        href={route}
+                        key={label}
+                        className={cn(
+                          "flex gap-3 items-center py-4 max-lg:px-4 justify-start",
+                          {
+                            "bg-nav-focus border-r-4 border-orange-1": isActive,
+                          }
+                        )}
+                      >
+                        <Image
+                          src={imgURL}
+                          alt={label}
+                          height={24}
+                          width={24}
+                        />
+                        <p>{label}</p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,21 +4,23 @@ import { useEffect, useState } from "react";
 import { Input } from "./ui/input";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
+import { useDebounce } from "@/lib/useDebounce";
 
 const SearchBar = () => {
   const [search, setSearch] = useState("");
   const router = useRouter();
   const pathName = usePathname();
 
+  const debouncedValue = useDebounce(search, 500);
+
   useEffect(() => {
-    if (search) {
-      router.push(`/discover?search=${search}`);
-    } else if (!search && pathName === "/discover") {
+    if (debouncedValue) {
+      router.push(`/discover?search=${debouncedValue}`);
+    } else if (!debouncedValue && pathName === "/discover") {
       router.push(`/discover`);
     }
-  }, [router, pathName, search]);
+  }, [router, pathName, debouncedValue]);
 
-  
   return (
     <div className="relative mt-8 block">
       <Input
