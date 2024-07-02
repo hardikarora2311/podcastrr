72ccb1e37ce15a2ed39133fb78ef78e36e0ed6ad
Hardik Arora <hardikarora826@gmail.com>
2024-07-02 17:59:24 +0530

    podcast player

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "@/components/LeftSidebar";
 import MobileNav from "@/components/MobileNav";
+import PodcastPlayer from "@/components/PodcastPlayer";
 import RightSidebar from "@/components/RightSidebar";
 import { Toaster } from "@/components/ui/toaster";
 import Image from "next/image";
@@ -33,6 +34,7 @@ export default function RootLayout({
         </section>
         <RightSidebar />
       </main>
+      <PodcastPlayer/>
     </div>
   );
 }
